fbcb4166e87ba3d1170791429fe7c50e4958390e
Alever Lai <759636538@qq.com>
2024-02-17 17:49:13 +0800

    [update] Update Button to prevent default click event.

--- src/packages/button/source/index.vue
+++ src/packages/button/source/index.vue
@@ -46,6 +46,7 @@ import { ClassBuilder, StyleBuilder, useTheme } from "@/utils/common";
 
 export default {
     name: "FvButton",
+    emits:['click'],
     props: {
         ...buttonProps,
         icon: {
