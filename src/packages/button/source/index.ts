c9e796be1fb1fc63af498d86db4645052a1b8f29
minskiter <minskiter@qq.com>
2024-06-04 23:23:34 +0800

    style(fv-button): modify code to typescript

--- src/packages/button/source/index.ts
+++ src/packages/button/source/index.ts
@@ -1,17 +1,105 @@
 import { commonProps } from "@/packages/common/props";
 import { computed, ExtractPropTypes, ref } from "vue";
 import { EmitFn } from "@/types/components";
+import { useTheme } from "@/utils/common";
 
 export const buttonProps = {
 	...commonProps,
+	icon: {
+		type: [String],
+		default: "",
+	},
+	foreground: {
+		type: [String],
+		default: "",
+	},
+	background: {
+		type: [String],
+		default: "",
+	},
+	borderRadius: {
+		type: [String, Number],
+		default: 3,
+	},
+	borderColor: {
+		type: [String],
+		default: "",
+	},
+	fontSize: {
+		type: [Number],
+		default: 13.3,
+	},
+	fontWeight: {
+		type: [String],
+		default: "normal",
+	},
+	revealBorderColor: {
+		type: [Boolean],
+		default: false,
+	},
+	revealBackgroundColor: {
+		type: [Boolean],
+		default: false,
+	},
+	isBoxShadow: {
+		type: [Boolean],
+		default: false,
+	},
+	disabled: {
+		type: [Boolean, String],
+		default: false,
+	},
+	borderWidth: {
+		type: [Number],
+		default: 1,
+	},
 };
 
 export type ButtonProps = ExtractPropTypes<typeof buttonProps>;
 
-export const buttonEmits = {};
+export const buttonEmits = {
+	click: (evt: Event)=>{
+		return true;
+	}
+};
 
 export type ButtonEmits = typeof buttonEmits;
 
 export const useButton = (props: ButtonProps, emits: EmitFn<ButtonEmits>) => {
-	return {};
+	const { theme } = useTheme(props)
+	const isDisabled = computed(()=>{
+		return props.disabled.toString()==="true" || props.disabled===true || props.disabled==="" || props.disabled==="disabled"
+	})
+	const onClick = (evt:Event)=>{
+		evt.preventDefault()
+		if (isDisabled.value){
+			return false;
+		}
+		emits("click",evt);
+	}
+	const borderLightColor = computed(()=>{
+		if (props.revealBorderColor) return props.revealBorderColor;
+		if (theme.value==="light"){
+			return "rgba(121, 119, 117, 0.6)";
+		}
+		if (theme.value === "dark"){
+			return "rgba(255,255,255,0.6)";
+		}
+	})
+	const backgroundLightColor = computed(()=>{
+		if (props.revealBackgroundColor) return props.revealBackgroundColor
+		if (theme.value === "light") {
+			return "rgba(121, 119, 117, 0.1)";
+		}
+		if (theme.value ==="dark") {
+			return "rgba(255, 255, 255, 0.1)";
+		}
+	})
+	return {
+		theme,
+		isDisabled,
+		onClick,
+		borderLightColor,
+		backgroundLightColor
+	};
 };
--- src/packages/button/source/index.vue
+++ src/packages/button/source/index.vue
@@ -1,137 +1,37 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+import { buttonProps, buttonEmits, useButton } from ".";
+
+defineOptions({
+    name: "FvButton"
+})
+
+const emits = defineEmits(buttonEmits);
+
+const props = defineProps(buttonProps);
+
+const { isDisabled, theme, onClick, backgroundLightColor, borderLightColor } = useButton(props, emits);
+
+const root = ref<HTMLElement>();
+
+</script>
+
 <template>
-    <div
-        class="fv-Button"
-        :class="[$theme]"
-        @click="onClick"
-    >
-        <div
-            class="fv-button-main-container"
-            :class="[{disabled: isDisabled}, { shadow: isBoxShadow }]"
-            :style="{borderRadius: `${borderRadius}px`}"
-        >
-            <fv-reveal-container
-                :parent="() => $el"
-                class="fv-button-reveal-container"
-                :backgroundColor="backgroundLightColor"
-                :borderColor="borderLightColor"
-                :borderGradientSize="80"
-                :borderWidth="borderWidth"
-                :borderRadius="borderRadius"
-                :disabled="isDisabled"
-            ></fv-reveal-container>
-            <div
-                :disabled="disabled"
-                class="fv-button-container"
-                :style="{background: background, 'background-repeat': 'no-repeat', 'background-clip': 'content-box', borderColor: borderColor, borderWidth: `${borderWidth}px`, borderRadius: `${borderRadius}px`}"
-            >
-                <span
-                    class="fv-btn-content-block"
-                    :style="{color: foreground, 'font-size': `${fontSize}px`, 'font-weight': fontWeight}"
-                >
-                    <i
-                        class="ms-Icon"
-                        :class="`ms-Icon--${icon}`"
-                        :style="{'margin-right': icon != '' ? '5px': ''}"
-                    ></i>
+    <div ref="root" class="fv-Button" :class="[theme]" @click="onClick">
+        <div class="fv-button-main-container" :class="[{ disabled: isDisabled }, { shadow: isBoxShadow }]"
+            :style="{ borderRadius: `${borderRadius}px` }">
+            <fv-reveal-container :parent="() => root" class="fv-button-reveal-container"
+                :backgroundColor="backgroundLightColor" :borderColor="borderLightColor" :borderGradientSize="80"
+                :borderWidth="borderWidth" :borderRadius="borderRadius" :disabled="isDisabled"></fv-reveal-container>
+            <div :disabled="disabled" class="fv-button-container"
+                :style="{ background: background, 'background-repeat': 'no-repeat', 'background-clip': 'content-box', borderColor: borderColor, borderWidth: `${borderWidth}px`, borderRadius: `${borderRadius}px` }">
+                <span class="fv-btn-content-block"
+                    :style="{ color: foreground, 'font-size': `${fontSize}px`, 'font-weight': fontWeight }">
+                    <i class="ms-Icon" :class="`ms-Icon--${icon}`"
+                        :style="{ 'margin-right': icon != '' ? '5px' : '' }"></i>
                     <slot>Button</slot>
                 </span>
             </div>
         </div>
     </div>
 </template>
-
-<script>
-import { buttonProps } from ".";
-import { ClassBuilder, StyleBuilder, useTheme } from "@/utils/common";
-
-export default {
-    name: "FvButton",
-    emits: ["click"],
-    props: {
-        ...buttonProps,
-        icon: {
-            default: "",
-            type: String,
-        },
-        foreground: {
-            default: "",
-            type: String,
-        },
-        background: {
-            default: "",
-            type: String,
-        },
-        borderRadius: {
-            default: 3,
-        },
-        borderColor: {
-            default: "",
-        },
-        fontSize: {
-            default: 13.3,
-        },
-        fontWeight: {
-            default: "normal",
-            type: String,
-        },
-        revealBorderColor: {
-            default: false,
-        },
-        revealBackgroundColor: {
-            default: false,
-        },
-        isBoxShadow: {
-            default: false,
-        },
-        disabled: {
-            default: false,
-        },
-        borderWidth: {
-            default: 1,
-        },
-    },
-    data() {
-        return {};
-    },
-    watch: {},
-    computed: {
-        $theme() {
-            return useTheme(this.$props).theme.value;
-        },
-        borderLightColor() {
-            if (this.revealBorderColor) return this.revealBorderColor;
-            if (this.$theme == "light") {
-                return "rgba(121, 119, 117, 0.6)";
-            }
-            if (this.$theme == "dark" || this.$theme == "custom") {
-                return "rgba(255, 255, 255, 0.6)";
-            }
-        },
-        backgroundLightColor() {
-            if (this.revealBackgroundColor) return this.revealBackgroundColor;
-            if (this.$theme == "light") {
-                return "rgba(121, 119, 117, 0.1)";
-            }
-            if (this.$theme == "dark" || this.$theme == "custom") {
-                return "rgba(255, 255, 255, 0.1)";
-            }
-        },
-        isDisabled() {
-            return (
-                this.disabled.toString() == "true" ||
-                this.disabled == "disabled" ||
-                this.disabled === ""
-            );
-        },
-    },
-    mounted() {},
-    methods: {
-        onClick(event) {
-            event.preventDefault();
-            if (this.isDisabled) return 0;
-            this.$emit("click", event);
-        },
-    },
-    beforeUnmount() {},
-};
-</script>
