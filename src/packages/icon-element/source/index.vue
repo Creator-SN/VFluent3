3cc7ad3fe559a76474f6575e7b492e10e1031243
minskiter <minskiter@qq.com>
2024-07-06 16:19:03 +0800

    [update] Updated IconElement Component

--- src/packages/icon-element/source/index.vue
+++ src/packages/icon-element/source/index.vue
@@ -1,88 +1,45 @@
+<script setup lang="ts">
+import { callFunction } from '@/utils/common';
+import { iconElementEmits, iconElementProps, useIconElement } from '.';
+
+defineOptions({
+    name:"FvIconElement"
+})
+
+const props = defineProps(iconElementProps)
+const emits = defineEmits(iconElementEmits)
+
+const {theme,iconList} = useIconElement(props, emits)
+
+</script>
+
 <template>
     <div
         class="fv-IconElement"
-        :class="[$theme, {'icon-element-btn': iconButton}]"
+        :class="[theme, {'icon-element-btn': iconButton}]"
         :style="{width: `${fontSize + paddingWidth / 1}px`, height: `${fontSize + paddingWidth / 1}px`, borderRadius: `${borderRadius}px`}"
         @click="$emit('click', $event)"
     >
         <div
             v-for="(item, index) in iconList"
             class="icon-element-item"
-            :class="[valueTrigger(item.position)]"
+            :class="[callFunction(item.position)]"
             :key="index"
         >
             <i
                 class="icon-element ms-Icon"
-                :class="[`ms-Icon--${valueTrigger(item.name)}`]"
+                :class="[`ms-Icon--${callFunction(item.name)}`]"
                 :style="{fontSize: `${fontSize}px`,
-                        color: valueTrigger(item.color),
-                        'background-image': valueTrigger(item.color),
+                        color: callFunction(item.color),
+                        'background-image': callFunction(item.color),
                         'background-clip': 'text',
-                        '-webkit-background-image': valueTrigger(item.color),
                         '-webkit-background-clip': 'text',
-                        '-webkit-text-fill-color': valueTrigger(item.color)
+                        '-webkit-text-fill-color': callFunction(item.color)
             }"
             ></i>
         </div>
     </div>
 </template>
         
-<script>
-import { iconElementProps } from '.';
-import { ClassBuilder, StyleBuilder, useTheme } from '@/utils/common';
 
-export default {
-    name: 'FvIconElement',
-    emits: ['click'],
-    props: {
-        ...iconElementProps,
-        value: {
-            default: 'AppIconDefault'
-        },
-        fontSize: {
-            default: 32
-        },
-        paddingWidth: {
-            default: 2
-        },
-        iconButton: {
-            type: Boolean,
-            default: false
-        },
-        borderRadius: {
-            default: 6
-        }
-    },
-    data() {
-        return {};
-    },
-    computed: {
-        iconList() {
-            let result = [];
-            if (Array.isArray(this.value)) result = this.value;
-            else result = this.value.split(',');
-            return result.map((item) => this.computeIconItem(item));
-        },
-        $theme() {
-            return useTheme(this.$props).theme.value;
-        }
-    },
-    methods: {
-        computeIconItem(item) {
-            let defaultItem = {
-                name: '',
-                color: this.$theme == 'dark' ? '#fff' : '#000',
-                position: 0
-            };
-            if (item.name) return Object.assign(defaultItem, item);
-            defaultItem.name = item;
-            return defaultItem;
-        },
-        valueTrigger(val) {
-            if (typeof val === 'function') return val();
-            return val;
-        }
-    }
-};
-</script>
 
