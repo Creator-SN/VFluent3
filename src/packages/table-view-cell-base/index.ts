4dde853717da420db05e7dca2ae0de1efd00067f
Alever <759636538@qq.com>
2025-11-21 19:59:35 +0800

    [update] Update TableViewCellBase props.

--- src/packages/table-view-cell-base/index.ts
+++ src/packages/table-view-cell-base/index.ts
@@ -1,5 +1,7 @@
 import { convertPlugin } from "@/utils/plugins/install-component"
 import "./style"
 import TableViewCellBase from "./source/index.vue"
+import { tableViewCellProps } from "./source/index"
+export const FvTableViewCellProps = tableViewCellProps
 export const FvTableViewCellBase = convertPlugin(TableViewCellBase)
 export default FvTableViewCellBase
--- src/packages/table-view-head-base/index.ts
+++ src/packages/table-view-head-base/index.ts
@@ -1,5 +1,7 @@
 import { convertPlugin } from "@/utils/plugins/install-component"
 import "./style"
 import TableViewHeadBase from "./source/index.vue"
+import { tableViewHeadProps } from "./source/index"
+export const FvTableViewHeadProps = tableViewHeadProps
 export const FvTableViewHeadBase = convertPlugin(TableViewHeadBase)
 export default FvTableViewHeadBase
